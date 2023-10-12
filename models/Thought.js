772ed069644f946f35c9bddc89ebd2edd52a087f
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-12 18:33:06 -0500

    update to Thoughts.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,39 @@
 const {Schema, model} = require("mongoose");
 const reaction = require("./Reaction");
+const reactionSchema = require("./Reaction");
 
+dateFormat= (dateTime) => {
+    return dateTime.toLocaleString();
+}
+
+const thoughtSchema = new Schema(
+    {
+        thoughText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        creatdAt: {
+            type: Date,
+            default: Date.now,
+            get: dateFormat
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+thoughtSchema.virtual(ThoughtCount)
+
+const Thought = model("thoughtCount").get(function () {
+    return this.reactions.length; 
+});

5abdf0c97cfc382b570a2b723afa7d5c0bae549b
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-18 18:25:19 -0500

    update to usercontrol.js

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -4,12 +4,39 @@ module.exports = {
     async getAllUsers(req, res) {
         try {
             const users = await User.find();
-            res.json(users);
+            res.status(202).json(users);
         } catch (error) {
-            res.status(500).json(error)
+            res.status(404).json(error)
         }
     },
     async getOneUser (req, res) {
-        
-    }
+        try {
+            const user = User.findOne({_id: req.params.id}).populate({path: "thoughts", select: "-__v"})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(404).json(error)
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const newUser =User.create(req.body)
+            res.status(200).json(newUser)
+        } catch (error) {
+            res.status(404).json(error)
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = User.findByIdAndUpdate(
+                {_id: req.params.id},
+                {$set: req.body},
+                {new: true, runValidators: true}
+            )
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(404).json(error)
+        }
+    },
+
+
 }

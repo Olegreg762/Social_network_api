16fa79d9708532568baaac9687bfe42f7dd90847
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-18 20:29:22 -0500

    another update to userControl.js

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,4 +1,5 @@
 const {User, Thought} = require('../models');
+const { findOneAndDelete } = require('../models/User');
 
 module.exports = {
     async getAllUsers(req, res) {
@@ -6,7 +7,7 @@ module.exports = {
             const users = await User.find();
             res.status(202).json(users);
         } catch (error) {
-            res.status(404).json(error)
+            res.status(500).json(error)
         }
     },
     async getOneUser (req, res) {
@@ -14,7 +15,7 @@ module.exports = {
             const user = User.findOne({_id: req.params.id}).populate({path: "thoughts", select: "-__v"})
             res.status(200).json(user)
         } catch (error) {
-            res.status(404).json(error)
+            res.status(500).json(error)
         }
     },
     async createUser(req, res) {
@@ -22,21 +23,40 @@ module.exports = {
             const newUser =User.create(req.body)
             res.status(200).json(newUser)
         } catch (error) {
-            res.status(404).json(error)
+            res.status(500).json(error)
         }
     },
     async updateUser(req, res) {
         try {
-            const user = User.findByIdAndUpdate(
+            const user = User.findByOneAndUpdate(
                 {_id: req.params.id},
                 {$set: req.body},
                 {new: true, runValidators: true}
             )
             res.status(200).json(user)
         } catch (error) {
-            res.status(404).json(error)
+            res.status(500).json(error)
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = findOneAndDelete({_id: req.param.id})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error)
         }
     },
 
-
+    async addFriend(req, res) {
+        try {
+            const friend = User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$addToSet: {friends: req.params.friendId}},
+                {new: true}
+            )
+            res.status(200).json(friend)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
 }

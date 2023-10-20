a3e1f35fe7677421ed28d4267b7997610494b670
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-19 20:28:48 -0500

    update to index.js & userRoutes.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router;
 const userRoutes = require('./api/userRoutes');
 const thoughtRoutes = require('./api/thoughtRoutes');
 
-router.use('/user', userRoutes);
-router.use('/thought', thoughtRoutes);
+router.use('/api/user', userRoutes);
+router.use('/api/thought', thoughtRoutes);
 
 router.use((req, res) => res.send("Invalid Route!"));
 
